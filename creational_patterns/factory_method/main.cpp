d7f78299eac6ca1a9bb95723cd0746c317f13f54
takaakit <takaaki.teshima.dev@gmail.com>
2022-04-02 07:48:20 +0900

    Fixed typo

--- creational_patterns/factory_method/main.cpp
+++ creational_patterns/factory_method/main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 /*
-The subject is a factory to make credit cards. The Factory defines how to create an credit card,
+The subject is a factory to make credit cards. The Factory defines how to create a credit card,
 but the actual credit card is created by the CreditCardFactory.
 The "createProduct()" is called a Factory Method, and it is responsible for manufacturing an object.
  */
